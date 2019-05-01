cf9c8b8c96eb5089909f3edbafd3e77196c7d7a6
Niklas Heyne <niklas.heyne@rub.de>
2019-05-01 19:04:05 +0200

    laid foundation for Shape class and its derivatives

--- easytikz-modelling.cpp
+++ easytikz-modelling.cpp
@@ -1,1 +1,53 @@
 #include "easytikz-modelling.h"
+#include <string>
+
+using namespace std;
+
+float Shape::getRootCoordX(){
+	return 4.5;
+}
+
+float Shape::getRootCoordY()
+{
+	return this->rootCoordY;
+}
+
+void Shape::setRootCoordX(float input)
+{
+	rootCoordX = input;
+}
+void Shape::setRootCoordY(float input)
+{
+	rootCoordY = input;
+}
+
+// Rectangle
+
+float Rectangle::getSize()
+{
+	return size;
+}
+int Rectangle::getVerts()
+{
+	return verts;
+}
+
+void Rectangle::setSize(float input)
+{
+	size = input;
+}
+void Rectangle::setVerts(int input)
+{
+	verts = input;
+}
+
+Rectangle::Rectangle()
+{
+	setRootCoordX(0);
+	setRootCoordY(0);
+}
+Rectangle::Rectangle(float rootCoordX, float rootCoordY)
+{
+	setRootCoordX(rootCoordX);
+	setRootCoordY(rootCoordY);
+}
--- easytikz-modelling.h
+++ easytikz-modelling.h
@@ -1,42 +1,3 @@
-
-class Shape
-{
-private:
-    /* data */
-public:
-    Shape(/* args */);
-    ~Shape();
-};
-
-Shape::Shape(/* args */)
-{
-}
-
-Shape::~Shape()
-{
-}
-
-class Connection
-{
-private:
-    /* data */
-public:
-    Connection
-(/* args */);
-    ~Connection
-();
-};
-
-Connection::Connection(/* args */)
-{
-}
-
-Connection::~Connection()
-{
-}
-
-
-
 class Diagram
 {
 private:
@@ -57,3 +18,56 @@ Diagram::Diagram(/* args */)
 Diagram::~Diagram()
 {
 }
+
+class Shape
+{
+private:
+    float rootCoordX;
+    float rootCoordY;
+protected:
+    
+public:
+    Shape(/* args */);
+    ~Shape();
+    float getRootCoordX();
+    virtual string toTikZ() = 0;
+    float getRootCoordX();
+    float getRootCoordY();
+    void setRootCoordX(float input);
+    void setRootCoordY(float input);
+};
+
+class Connection
+{
+private:
+    /* data */
+public:
+    Connection(/* args */);
+    ~Connection();
+    
+
+};
+//=======================================================
+//==== Shape derivatives ================================
+//=======================================================
+//=======================================================
+
+class Rectangle:Shape
+{
+	private:
+
+	protected:
+		int verts;
+		float size;
+		//group of float[2][1] arrays for connection hardpoints?
+
+	public:
+		float getSize();
+		int getVerts();
+		void setSize(float input);
+		void setVerts(int input);
+		Rectangle();
+
+		Rectangle(float rootCoordX, float rootCoordY);
+		
+};
