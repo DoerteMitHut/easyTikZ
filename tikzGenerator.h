ecb3d36810dfed85124c3ea3d8abae84e7617a63
Niklas Heyne <niklas.heyne@rub.de>
2019-05-01 19:28:55 +0200

    Robin: implemented TikZ-output of quadrilaterals

---
+++ tikzGenerator.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+class TikzGenerator
+{
+	private:
+		string stringDigital;
+
+		string drawPolygon(int verts, float size, float rootCoordX, float rootCoordY);
+		int printEasyTikZ(string stringToPrint, string pathOutput);
+	public:
+		int generateEasyTikZ(string diagramInput, string pathOutput);
+		void debugMethod();
+};
